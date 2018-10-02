f5e5b7aeb2631ee05730baeb6edaa712b4d16b6c
ishak14 <andy.ishak10@gmail.com>
2018-10-02 14:21:51 +0000

    adding the metadata class

---
+++ classes/metadata_class.py
@@ -0,0 +1,26 @@
+class Ingredient():
+	def __init__(self,name,amount):
+		self.name = name
+		self.amount = amount
+class Description():
+	def __init__(self,description):
+		self.description = description
+class Author():
+	def __init__(self,name,institute):
+		self.name = name
+		self.institute = institute
+
+class Metadata():
+	self.ingredient = None
+	self.description = None
+	self.authors = [Author() for _ in range(100)]
+
+	def __init__(self,
+			ingredient.name
+			ingredient.amount,
+			description,
+			authors)
+		self.ingredient.name = ingredient.name
+		self.ingredient.amount = ingredient.amount
+		self.description = description
+		self.authors = [Author() for _ in range(100)]
