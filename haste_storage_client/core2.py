a8aceae5178b92cb1a79cfe3e688c6d7aa375b9e
ishak14 <andy.ishak10@gmail.com>
2018-10-04 19:55:53 +0200

    Testing

--- haste_storage_client/core2.py
+++ haste_storage_client/core2.py
@@ -62,10 +62,10 @@ class HasteStorageClientMeta:
 			blob_id = 'strm_' + self.project_id + '_ts_' + str(timestamp)
 			document = {'timestamp': timestamp,
 				'substream':{
-					'substream_id': substream_id,
-					'info': {
-							'project_name': project.name,
-							'project_description': project.description,
+					'id': substream_id,
+					'project': {
+							'name': project.name,
+							'description': project.description,
 							'author': {
 											'name': project.metadata.authors[1].name,
 											'insitute': project.metadata.authors[1].institute
